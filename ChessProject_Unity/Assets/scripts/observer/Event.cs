11a7861ad99209f68c2c8323fc43385c73e22531
Simon <simon@pc15.home>
2016-06-18 13:26:34 +0200

    chessproject stash

---
+++ ChessProject_Unity/Assets/scripts/observer/Event.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public enum EventType { None = 0, Timeout = 1, MovementDone = 2, EndTurn = 3}
+
+/// <summary>
+/// An Event class with all the informations on
+/// the event that you receive
+/// </summary>
+public class Event
+{
+    private EventType type;             //type of the event
+    public EventType Type
+    {
+        get
+        {
+            return type;
+        }
+    }
+
+    private String message;             //message which is sent/received with the event
+    public String Message
+    {
+        get
+        {
+            return message;
+        }
+            
+    }
+
+    public Event(EventType type, String message)
+    {
+        this.type = type;
+        this.message = message;
+    }
+}
